8090de236344885d9056ddedf423fc82e82f6eca
RaoAsadMehmood <asadrao2489@gmail.com>
2025-01-01 03:28:46 +0500

    Changes in card

--- src/app/components/ClothsCard.tsx
+++ src/app/components/ClothsCard.tsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-
-// interface CardProps {
-//     imageSrc: string;
-// }
-
-// const ClothsCard: React.FC<CardProps> = ({ imageSrc }) => {
-//     return (
-//         <div className="w-[206px] h-[233px] relative overflow-hidden group transition-all duration-300 ease-in-out shadow-md hover:shadow-lg">
-//             {/* Card Image Container */}
-//             <div className="w-full h-full">
-//                 <img
-//                     src={imageSrc}
-//                     alt="Card Image"
-//                     className="w-full h-full object-fit transition-transform duration-300 ease-in-out group-hover:scale-110"
-//                     width={206}
-//                     height={233}
-//                 />
-//             </div>
-
-//             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center 
-//                 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
-//                 <h3 className="text-white font-bold text-xl transform translate-y-4 group-hover:translate-y-0 transition-transform duration-300 ease-in-out">
-//                     Cloths Card
-//                 </h3>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default ClothsCard;
-
 import React from 'react';
 
 interface CardProps {
